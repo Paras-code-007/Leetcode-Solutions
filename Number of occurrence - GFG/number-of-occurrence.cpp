12aba19ab6b7e17de81b472ef6b059ecb566f755
Paras-code-007 <56780209+Paras-code-007@users.noreply.github.com>
2022-10-12 04:01:28 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -12,12 +12,87 @@ public:
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
 	int count(int a[], int n, int x) {
-	    int ct=0;
-	    for(int i=0;i<n;i++){
-	        if(a[i]==x) ct++;
-	    }
+	   // int ans=0;
 	    
-	    return ct;
+	   // int ct1=0;
+	   // int st=0;
+	   // int end= n-1;
+	   // while(st<=end){
+	   //     int mid= st+ (end-st)/2;
+	   //     if(a[mid]==x){
+	   //         ct1++;
+	   //         end= mid-1;
+	   //     }else if(x<a[mid]){
+	   //         end= mid-1;
+	   //     }else{
+	   //         st= mid+1;
+	   //     }
+	   // }
+	   //// cout<<ct1<<endl;
+	    
+	   // int ct2=0;
+	   // st=0;
+	   // end= n-1;
+	   // while(st<=end){
+	   //     int mid= st+ (end-st)/2;
+	   //     if(a[mid]==x){
+	   //         ct2++;
+	   //         st= mid+1;
+	   //     }else if(x<a[mid]){
+	   //         end= mid-1;
+	   //     }else{
+	   //         st= mid+1;
+	   //     }
+	   // }
+	   //// cout<<ct1<<endl;
+	   // ans= ct1+ct2;
+	    
+    //     if(ans%2=0){
+    //         ans--;
+    //     }
+	   // return ans;
+	   
+	   //first occurence
+	   
+	   int ans=0;
+	   int fa=-1;
+	   int st=0;
+	   int end=n-1;
+	   
+	   while(st<=end){
+	       int mid= st+ (end -st)/2;
+	       if(a[mid]==x){
+	           fa= mid;
+	           end= mid-1;
+	       }else if(x<a[mid]){
+	           end= mid-1;
+	       }else{
+	           st= mid+1;
+	       }
+	   }
+	   
+	   int la=-1;
+	   st=0;
+	   end=n-1;
+	   
+	   while(st<=end){
+	       int mid= st+ (end -st)/2;
+	       if(a[mid]==x){
+	           la= mid;
+	           st= mid+1;
+	       }else if(x<a[mid]){
+	           end= mid-1;
+	       }else{
+	           st= mid+1;
+	       }
+	   }
+	   
+	   if(fa==-1 || la==-1){
+	       return ans;
+	   }
+	   ans= la-fa+1;
+	   
+	   return ans;
 	}
 };
 
